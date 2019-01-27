cf4cc1bfdd74808a2fae6e7e1d6445585f828b5c
Neremeev <n.eremeev92@mail.ru>
2019-01-27 16:14:23 +0300

    lesson 29

--- src/app/system/history-page/history-detail/history-detail.component.ts
+++ src/app/system/history-page/history-detail/history-detail.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {EventsService} from "../../shared/services/events.service";
+import {CategoriesService} from "../../shared/services/categories.service";
+import {mergeMap} from "rxjs/operators";
+import {MyEvent} from "../../shared/models/event.models";
+import {Category} from "../../shared/models/category.models";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-history-detail',
   templateUrl: './history-detail.component.html',
   styleUrls: ['./history-detail.component.scss']
 })
-export class HistoryDetailComponent implements OnInit {
+export class HistoryDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  event: MyEvent;
+  category: Category;
+
+  isLoaded = false;
+
+  s1: Subscription;
+
+
+  constructor(private route: ActivatedRoute,
+              private eventServise: EventsService,
+              private categoriesService: CategoriesService) {
+
+  }
 
   ngOnInit() {
+
+    this.s1 = this.route.params.pipe(
+        mergeMap((params: Params) => this.eventServise.getEventById(params['id'])),
+        mergeMap((event:MyEvent) => {
+          this.event = event;
+          return this.categoriesService.getCategoryById(event.category);
+        })
+    ).subscribe((category) => {
+      this.category = category;
+      this.isLoaded = true;
+    })
+
+  }
+
+  ngOnDestroy() {
+    if(this.s1) {
+      this.s1.unsubscribe();
+    }
   }
 
 }
